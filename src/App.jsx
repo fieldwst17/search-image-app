cfdbb2caf096fcabb0ff93b26f105e177ef2bff4
fieldwst17 <149990079+fieldwst17@users.noreply.github.com>
2024-02-21 11:55:03 +0700

    Picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Picture from "./components/picture";
+import Picture from "./components/Picture";
 
 function App() {
   const [word, setWord] = useState("");
